c55a465a182833d7b35d87d3dc4c7049295c8d3a
RyanDawkins <ryan@lifechurch.tv>
2014-02-08 00:40:34 -0600

    Updated the request class to parse json strings and created exceptions to throw in them

--- src/com/ryanddawkins/glowing-spice/Request.java
+++ src/com/ryanddawkins/glowing-spice/Request.java
@@ -1,6 +1,13 @@
 package com.ryanddawkins.glowing_spice;
 
-import com.ryanddakwins.glowing_spice.Response;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+
+import com.ryanddawkins.glowing_spice.Response;
+import com.ryanddawkins.glowing_spice.NullJsonException;
+import com.ryanddawkins.glowing_spice.JsonCommandNotFoundException;
 
 /**
  * Class to handle and deal with the various commands sent to the server.
@@ -14,25 +21,99 @@ public class Request
 
 	private String command;
 	private Response response;
+	private String json;
 
 	/**
-	 * 
+	 * Takes JSON String and decides what to do in the parse
 	 *
-	 *
+	 * @param String json
 	 */
-	public Request(String command)
+	public Request(String json)
 	{
-		this.command = command;
+		this.json = json;
+		this.command = null;
+		this.response = null;
 	}
 
+	/**
+	 * Class to parse the JSON string into useable data
+	 *
+	 * @throws NullJsonException
+	 * @throws JsonCommandNotFoundException
+	 * @return Request this
+	 */
+	public Request parse() throws NullJsonException, JsonCommandNotFoundException
+	{
+		JsonElement parentElement = new JsonParser().parse(this.json);
+		if(parentElement.isJsonObject())
+		{
+			JsonObject jobject = parentElement.getAsJsonObject();
+			if(jobject.has("command"))
+			{
+				this.command = jobject.get("command").getAsString();
+			}
+		}
+		else if(parentElement.isJsonArray())
+		{
+			JsonArray jarray = parentElement.getAsJsonArray();
+			JsonObject jobject = getCommandJsonObject(jarray);
+			if(jobject != null && jobject.has("command"))
+			{
+				this.command = jobject.getAsString();
+			}
+			else
+			{
+				throw new JsonCommandNotFoundException("Json Command not Found");
+			}
+		}
+		else if(parentElement.isJsonNull())
+		{	
+			throw new NullJsonException("Parent node in JSON is null");
+		}
+		return this;
+	}
+
+	/**
+	 * Recursive method to find the command json object
+	 *
+	 * @param JsonArray je
+	 * @return JsonObject commandObject
+	 */
+	public JsonObject getCommandJsonObject(JsonArray je)
+	{
+		for(int x = 0; x < je.size(); x++)
+		{
+			if(je.isJsonObject() && je.getAsJsonObject().has("command"))
+			{
+				return je.getAsJsonObject();
+			}
+			else if(je.isJsonArray())
+			{
+				return getCommandJsonObject(je);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Traditional getter to grab which command
+	 *
+	 * @return String command
+	 */
 	public String getCommand()
 	{
 		return this.command;
 	}
 
-	public void runCommand()
+	/**
+	 * Sets command to the object
+	 *
+	 * @param String command
+	 * @return Request
+	 */
+	public Request setCommand(String command)
 	{
-	`
+		this.command = command;
+		return this;
 	}
-
 }
