0753df6a753d74876a2d04f9f88f282447fbf6b6
RyanDawkins <ryan@lifechurch.tv>
2014-02-10 23:41:32 -0600

    Added functionality to pause, play, and list movies

--- src/com/ryanddawkins/glowing-spice/VideoPlayer.java
+++ src/com/ryanddawkins/glowing-spice/VideoPlayer.java
@@ -2,9 +2,9 @@ package com.ryanddawkins.glowing_spice;
 
 import javax.swing.JFrame;
 import java.awt.Toolkit;
-
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
-
 import com.sun.jna.Native;
 
 /**
@@ -15,30 +15,56 @@ import com.sun.jna.Native;
  * @since 0.1
  * @extends javax.swing.JFrame
  */
-
 public class VideoPlayer extends JFrame
 {
 
-    public static void main(String[] args)
-    {
-	VideoPlayer v = new VideoPlayer();
-    }
-
+	private String playingFile;
     private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 
+    /**
+     * Constructor that creates the JFrame
+     */
     public VideoPlayer()
     {
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Sets fullscreen
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		int xsize = ((int) tk.getScreenSize().getWidth());
+		int ysize = ((int) tk.getScreenSize().getHeight());
+		this.setSize(xsize, ysize);
+		
+        // Grabs mediaComponent to call functions on
+		mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+		this.setContentPane(this.mediaPlayerComponent);
 
-	Toolkit tk = Toolkit.getDefaultToolkit();
-	int xsize = ((int) tk.getScreenSize().getWidth());
-	int ysize = ((int) tk.getScreenSize().getHeight());
-	this.setSize(xsize, ysize);
-	
-	mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-	this.setContentPane(this.mediaPlayerComponent);
+        // This is so we can implement our own windowAdapter
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                setVisible(false);
+                pause();
+            }
+        });
 
-	this.setVisible(true);
-
+		this.setVisible(false);
     }
+
+    /**
+     * Chainable method to play the given file by string name
+     *
+     * @param String path filepath to video file
+     * @return VideoPlayer this
+     */
+    public VideoPlayer playFile(String path)
+    {
+    	this.mediaPlayerComponent.getMediaPlayer().playMedia(path);
+    	this.playingFile = path;
+    	return this;	
+    }
+
+    public VideoPlayer pause()
+    {
+        this.mediaPlayerComponent.getMediaPlayer().pause();
+        return this;
+    }
+
 }
